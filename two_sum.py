dbdfe474d6be8044ce1815642906e85c69ce7d33
Sagar Gosavi <sagargosavi1112@gmail.com>
2022-06-05 00:43:29 +0530

    two_sum

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+# Return indices of two numbers
+
+from colorsys import TWO_THIRD
+
+
+class Solution(object):
+    def twosum(self,l,n):
+        mydict = {}
+        for i,v in enumerate(l):
+            print(mydict)
+            if n-v in mydict:
+                return [mydict[n-v],i]
+
+            else:
+                mydict[v] = i
+
+        return []
+
+l = [10,20,10,40,50,60,70,80,90]
+n = 50
+x = Solution()
+a = x.twosum(l,n)
+print(a)
