bae5e6e92bda5584aea2b4f589a8f5eb2e8708fd
sagargosavi <sagargosavi1112@gmail.com>
2022-06-09 21:06:28 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -5,8 +5,6 @@ from colorsys import TWO_THIRD
 
 class Solution(object):
     def twosum(self,l,n):
-        print(n)
-        print(l)
         mydict = {}
         for i,v in enumerate(l):
             if n-v in mydict:
